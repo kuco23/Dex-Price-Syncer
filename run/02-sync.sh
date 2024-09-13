b88ab12a0c839a03e7714985529521e37d3e46d2
kuco23 <nseverkar@gmail.com>
2024-09-13 13:58:05 +0200

    better program output

--- run/02-sync.sh
+++ run/02-sync.sh
@@ -9,6 +9,14 @@ symbolB=$6
 maxSpentA=$7
 maxSpentB=$8
 
-cast send $DEX_PRICE_SYNCER_ADDRESS "sync(address,address,address,address,string,string,uint256,uint256)" \
-    $uniswapV2Router $priceReader $tokenA $tokenB $symbolA $symbolB $maxSpentA $maxSpentB \
-    --private-key $PRIVATE_KEY --rpc-url $RPC_URL
+ccall=$(
+    cast send $DEX_PRICE_SYNCER_ADDRESS "sync(address,address,address,address,string,string,uint256,uint256)" \
+        $uniswapV2Router $priceReader $tokenA $tokenB $symbolA $symbolB $maxSpentA $maxSpentB \
+        --private-key $PRIVATE_KEY --rpc-url $RPC_URL
+)
+
+if echo "$ccall" | grep -q "(success)"; then
+    echo "Syncing pool ($symbolA,$symbolB) succeeded"
+else
+    echo "Syncing pool ($symbolA,$symbolB) failed with error: $ccall"
+fi
